228968a95252fc1384639c17d3fe4b6e9ccfa76f
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-28 21:31:39 -0400

    albums child formulated

--- browser/react/containers/AppContainer.js
+++ browser/react/containers/AppContainer.js
@@ -108,19 +108,17 @@ export default class AppContainer extends Component {
         <div className="col-xs-2">
           <Sidebar deselectAlbum={this.deselectAlbum} />
         </div>
+
         <div className="col-xs-10">
         {
-          this.state.selectedAlbum.id ?
-          <Album
-            album={this.state.selectedAlbum}
-            currentSong={this.state.currentSong}
-            isPlaying={this.state.isPlaying}
-            toggleOne={this.toggleOne}
-          /> :
-          <Albums
-            albums={this.state.albums}
-            selectAlbum={this.selectAlbum}
-          />
+          this.props.children ? React.cloneElement(this.props.children, {
+            album: this.state.selectedAlbum,
+            currentSong: this.state.currentSong,
+            isPlaying: this.state.isPlaying,
+            toggleOne: this.toggleOne,
+            albums: this.state.albums,
+            selectAlbum: this.selectAlbum
+          }) : null
         }
         </div>
         <Player
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
+import {Router, Route, hashHistory} from 'react-router';
+import Albums from './components/Albums';
 
 ReactDOM.render(
-  <AppContainer />,
+  	<Router history={ hashHistory } >
+		<Route path='/' component={ AppContainer }>
+			<Route path='/albums' component= {Albums} />
+		</Route>
+    </Router>,
   document.getElementById('app')
 );
