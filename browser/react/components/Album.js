8fe20ae27f8ff4ad0d74ae48c6e04ff820838826
Robert van der Mark <robert.vandermark@gmail.com>
2017-03-29 22:21:54 -0400

    through Sidebar links working

--- browser/react/components/Album.js
+++ browser/react/components/Album.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Songs from '../components/Songs';
 
-const Album = (props) => {
 
-  const album = props.album;
-  const currentSong = props.currentSong;
-  const isPlaying = props.isPlaying;
-  const toggleOne = props.toggleOne;
 
+
+
+class Album extends React.Component{
+  constructor(props){
+    super();
+    this.state ={album: {}};
+
+  }
+
+ componentDidMount () {
+  const albumId = this.props.routeParams.albumId; //from the url id#
+  const selectAlbum = this.props.selectAlbum; //function to change app continor state
+  selectAlbum(albumId);
+}
+
+render(){
+  const album = this.props.selectedAlbum || {};
   return (
     <div className="album">
       <div>
@@ -16,11 +28,34 @@ const Album = (props) => {
       </div>
       <Songs
         songs={album.songs}
-        currentSong={currentSong}
-        isPlaying={isPlaying}
-        toggleOne={toggleOne} />
+        currentSong={this.props.currentSong}
+        isPlaying={this.props.isPlaying}
+        toggleOne={this.props.toggleOne} />
     </div>
   );
 }
+};
+
+// const Album = (props) => {
+
+//   const album = props.album;
+//   const currentSong = props.currentSong;
+//   const isPlaying = props.isPlaying;
+//   const toggleOne = props.toggleOne;
+
+//   return (
+//     <div className="album">
+//       <div>
+//         <h3>{ album.name }</h3>
+//         <img src={ album.imageUrl } className="img-thumbnail" />
+//       </div>
+//       <Songs
+//         songs={album.songs}
+//         currentSong={currentSong}
+//         isPlaying={isPlaying}
+//         toggleOne={toggleOne} />
+//     </div>
+//   );
+// }
 
 export default Album;
--- browser/react/index.js
+++ browser/react/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './containers/AppContainer';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, hashHistory, IndexRedirect} from 'react-router';
 import Albums from './components/Albums';
+import Album from './components/Album';
+import Artists from './components/Artists';
 
 ReactDOM.render(
   	<Router history={ hashHistory } >
 		<Route path='/' component={ AppContainer }>
+			<IndexRedirect to='/albums' />
 			<Route path='/albums' component= {Albums} />
+			<Route path="albums/:albumId" component={Album} />
+			<Route path="/artists" component={Artists} />
 		</Route>
     </Router>,
   document.getElementById('app')
